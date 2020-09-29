d6e799c33d21aee23f542739169d1b7c978fe827
IgorAleks88 <igor.alexeyenko@gmail.com>
2020-09-29 10:01:36 +0300

    feat: add solution to this task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    let minElement = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (minElement > array[i]) {
+            minElement = array[i];
+        }
+    }
+    return minElement;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    let maxElement = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (maxElement < array[i]) {
+            maxElement = array[i];
+        }
+    }
+    return maxElement;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    let sumOfElements = array[0];
+    for (let i = 1; i < array.length; i++) {
+        sumOfElements += array[i];
+    }
+    return sumOfElements / array.length;
+};
